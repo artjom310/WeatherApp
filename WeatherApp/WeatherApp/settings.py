4559864c34bcb96c24e6d8060673e19b93bbbd3e
Artem <artjom310@gmail.com>
2021-11-27 17:20:52 +0300

    weather app without validator of cities

--- WeatherApp/WeatherApp/settings.py
+++ WeatherApp/WeatherApp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'weather'
 ]
 
 MIDDLEWARE = [
@@ -66,6 +67,7 @@ TEMPLATES = [
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'WeatherApp.wsgi.application'
 
--- WeatherApp/weather/views.py
+++ WeatherApp/weather/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+import requests
+from .models import City
+from .forms import CityForm
+
 
 # Create your views here.
+def index(request):
+    appid = '9e0c6f373684bac58b0cd90eb636d91a'
+    url = r'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
+
+
+    if(request.method == 'POST'):
+        form = CityForm(request.POST)
+        form.save()
+
+    form = CityForm()
+
+
+    cities = City.objects.all()
+
+    all_cities = []
+    for city in cities:
+        res = requests.get(url.format(city.name)).json()
+        city_info = {
+            'city': city.name,
+            'temp': res['main']['temp'],
+            'icon': res['weather'][0]['icon'],       
+        }
+        all_cities.append(city_info)
+
+
+    contex = {'all_info': all_cities, 'form': form}
+    print(res)
+    return render(request, 'weather/index.html', contex)
